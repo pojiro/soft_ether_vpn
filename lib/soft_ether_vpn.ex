d8cfea1fc4d8b4c12af1ac068ec29b50477533dd
Ryota Kinukawa <kinukawa@tombo-works.com>
2023-07-28 16:12:30 +0900

    Initial commit

---
+++ lib/soft_ether_vpn.ex
@@ -0,0 +1,18 @@
+defmodule SoftEtherVpn do
+  @moduledoc """
+  Documentation for `SoftEtherVpn`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SoftEtherVpn.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/soft_ether_vpn_test.exs
@@ -0,0 +1,8 @@
+defmodule SoftEtherVpnTest do
+  use ExUnit.Case
+  doctest SoftEtherVpn
+
+  test "greets the world" do
+    assert SoftEtherVpn.hello() == :world
+  end
+end
