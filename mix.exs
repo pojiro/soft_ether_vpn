eb11fd6f3f90842db58cf4feb38c91c8f22ae096
Ryota Kinukawa <kinukawa@tombo-works.com>
2023-07-31 17:03:24 +0900

    Fix castore version range

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule SoftEtherVpn.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:castore, "~> 1.0"}
+      {:castore, "~> 0.1 or ~> 1.0"}
     ]
   end
 end
